a28bb2e4966922bd53e71edef01073e55c73f80d
Mike Kravtsov <mike4040@me.com>
2019-09-16 13:50:27 -0700

    basic search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,31 @@
+const centerInit = {lat: -33.8688, lng: 151.2195};
+let infowindow;
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
+    center: centerInit,
     zoom: 13,
     mapTypeId: 'roadmap'
   });
 
+  // var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
+
+  infowindow = new google.maps.InfoWindow();
+
+  var request = {
+    location: centerInit,
+    radius: '500',
+    type: ['restaurant']
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, searchCallback);
+
+
+
+
+  // -------------------------------
+        
   //Call geocoder function and center on submit once address placed
   var geocoder = new google.maps.Geocoder();
 
@@ -88,4 +109,13 @@ geocoder.geocode({'address': address}, function(results, status) {
     alert('Geocode was not successful for the following reason: ' + status);
   }
 });
+}
+
+function searchCallback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      console.log(results[i].name, 'Rating: ', results[i].rating);
+      // createMarker(results[i]);
+    }
+  }
 }
