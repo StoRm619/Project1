d0c59a6734a3edbc476f8a96bc8d0ac28f9c94c8
Mike Kravtsov <mike4040@me.com>
2019-09-17 14:43:06 -0700

    Besic search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,29 +2,22 @@ const centerInit = {lat: -33.8688, lng: 151.2195};
 let infowindow;
 let places = [];
 let currentAdd = '';
+let currentLatLng = {};
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  let map = new google.maps.Map(document.getElementById('map'), {
     center: centerInit,
     zoom: 13,
     mapTypeId: 'roadmap'
   });
-
-  var request = {
-    location: centerInit,
-    radius: '500',
-    type: ['restaurant']
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, searchCallback);
+  let service = new google.maps.places.PlacesService(map);
         
   //Call geocoder function and center on submit once address placed
   var geocoder = new google.maps.Geocoder();
 
   //submit id will be replaced by id of submit button on form
   document.getElementById('submit').addEventListener('click', function() {
-    geocodeAddress(geocoder, map);
+    // geocodeAddress(geocoder, map);
   });
 
   // Create the search box and link it to the UI element.
@@ -37,9 +30,10 @@ function initMap() {
     searchBox.setBounds(map.getBounds());
   });
 
-  // var markers = [];
+  
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
+
   searchBox.addListener('places_changed', function() {
     let currentLocInput = searchBox.getPlaces();
 
@@ -47,13 +41,25 @@ function initMap() {
       return;
 
     currentAdd = currentLocInput[0].formatted_address;
-    let currentGeo = currentLocInput[0].geometry
+    let currentGeo = currentLocInput[0].geometry;
     let bounds = new google.maps.LatLngBounds();
     if (currentGeo.viewport)
       bounds.union(currentGeo.viewport);
     else 
       bounds.extend(currentGeo.location);
     map.fitBounds(bounds);
+
+
+    geocodeAddress(currentAdd, function (latLng) {
+      let requestNearby = {
+        location: latLng,
+        radius: '500',
+        type: ['restaurant']
+      };
+      service.nearbySearch(requestNearby, searchCallback);
+    });
+
+
 
     
 
@@ -87,37 +93,41 @@ function initMap() {
     //   }));
 
   });
-}
 
-//Function to call when we need to get location and center
-function geocodeAddress(geocoder, resultsMap) {
-  //address id is going to be replaced by id of input on form
-var address = document.getElementById('address').value;
-geocoder.geocode({'address': address}, function(results, status) {
-  if (status === 'OK') {
-    resultsMap.setCenter(results[0].geometry.location);
-    var marker = new google.maps.Marker({
-      map: resultsMap,
-      position: results[0].geometry.location
-    });
-  } else {
-    alert('Geocode was not successful for the following reason: ' + status);
-  }
-});
-}
-
-function searchCallback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      let request = {
-        placeId: results[i].place_id,
-        fields: ['name', 'rating', 'formatted_address']
-      };
-      service.getDetails(request, makeDetailCallback(places));
+  function searchCallback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        let request = {
+          placeId: results[i].place_id,
+          fields: ['name', 'rating', 'formatted_address']
+        };
+        service.getDetails(request, makeDetailCallback(places));
+      }
+      console.log('SearchCallback, places:', places);
     }
-    console.log(places);
   }
 }
+
+
+// We've implemented this different way. Just saving for now.
+
+// //Function to call when we need to get location and center
+// function geocodeAddress(geocoder, resultsMap) {
+//   //address id is going to be replaced by id of input on form
+// var address = document.getElementById('address').value;
+// geocoder.geocode({'address': address}, function(results, status) {
+//   if (status === 'OK') {
+//     resultsMap.setCenter(results[0].geometry.location);
+//     var marker = new google.maps.Marker({
+//       map: resultsMap,
+//       position: results[0].geometry.location
+//     });
+//   } else {
+//     alert('Geocode was not successful for the following reason: ' + status);
+//   }
+// });
+// }
+
 
 // Modifies places by adding details to passin in placesToModify argument
 function makeDetailCallback(placesToModify) {
@@ -128,4 +138,17 @@ function makeDetailCallback(placesToModify) {
        address: results.formatted_address,
        rating: results.rating});
   }
+}
+
+function geocodeAddress(address, callback) {
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      currentLatLng = results[0].geometry.location;
+      console.log('Geocoding: ', currentLatLng);
+      callback(currentLatLng);
+    }
+    else
+      alert('Geocode was not successful for the following reason: ' + status);
+  });
 }
