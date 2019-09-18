9760e9d7c3011574196006f9fc9884407ac56bb0
Matthews A <matthewsabebe23@gmail.com>
2019-09-18 13:45:32 -0700

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,153 +6,152 @@ let service;
 let geocoder;
 
 function initMap() {
-  let map = new google.maps.Map(document.getElementById('map'), {
-    center: centerInit,
-    zoom: 13,
-    mapTypeId: 'roadmap'
-  });
+    let map = new google.maps.Map(document.getElementById('map'), {
+        center: centerInit,
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    });
 
-  service = new google.maps.places.PlacesService(map);
-  geocoder = new google.maps.Geocoder();
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
+    service = new google.maps.places.PlacesService(map);
+    geocoder = new google.maps.Geocoder();
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
 
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
 
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
 
-  searchBox.addListener('places_changed', function() {
-    let currentLocInput = searchBox.getPlaces();
+    searchBox.addListener('places_changed', function() {
+        let currentLocInput = searchBox.getPlaces();
 
-    if (currentLocInput.length == 0) return;
+        if (currentLocInput.length == 0) return;
 
-    currentAdd = currentLocInput[0].formatted_address;
-    console.log({currentAdd})
-    let currentGeo = currentLocInput[0].geometry;
+        currentAdd = currentLocInput[0].formatted_address;
+        console.log({ currentAdd })
+        let currentGeo = currentLocInput[0].geometry;
 
 
-    let bounds = new google.maps.LatLngBounds();
-    if (currentGeo.viewport) bounds.union(currentGeo.viewport);
-    else bounds.extend(currentGeo.location);
-    map.fitBounds(bounds);
+        let bounds = new google.maps.LatLngBounds();
+        if (currentGeo.viewport) bounds.union(currentGeo.viewport);
+        else bounds.extend(currentGeo.location);
+        map.fitBounds(bounds);
 
-    geocodeAddress(currentAdd, callSearchNearby);
-  });
+        geocodeAddress(currentAdd, callSearchNearby);
+    });
 }
 
 function callSearchNearby(latLng) {
-  let requestNearby = {
-    location: latLng,
-    radius: '8000', //in meters.
-    type: ['restaurant']
-  };
-  service.nearbySearch(requestNearby, callGetDetails);
+    let requestNearby = {
+        location: latLng,
+        radius: '3200', //in meters.
+        type: ['restaurant']
+    };
+    service.nearbySearch(requestNearby, callGetDetails);
 }
 
 function callGetDetails(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    // controle asyncronous place details requests with the following values
-    let detailsRequestCompletedCount = 0;
-    let allDetailsRequestsComplete = () =>
-    detailsRequestCompletedCount === results.length;
-    places = [];
-    for (var i = 0; i < results.length; i++) {
-      let request = {
-        placeId: results[i].place_id,
-        fields: ['name', 'rating', 'formatted_address']
-      };
-      // service.getDetails(request, makeDetailCallback(places));
-      service.getDetails(request, function(results, status) {
-        detailsRequestCompletedCount += 1;
-        console.log({detailsRequestCompletedCount})
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-          places.push({
-            name: results.name,
-            address: results.formatted_address,
-            rating: results.rating
-          });
-          console.log(places)
-        }
-        if (allDetailsRequestsComplete()) {
-          console.log('all details are done now. proceed.')
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        // controle asyncronous place details requests with the following values
+        let detailsRequestCompletedCount = 0;
+        let allDetailsRequestsComplete = () =>
+            detailsRequestCompletedCount === results.length;
+        places = [];
+        for (var i = 0; i < results.length; i++) {
+            let request = {
+                placeId: results[i].place_id,
+                fields: ['name', 'rating', 'formatted_address']
+            };
+            // service.getDetails(request, makeDetailCallback(places));
+            service.getDetails(request, function(results, status) {
+                detailsRequestCompletedCount += 1;
+                console.log({ detailsRequestCompletedCount })
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    places.push({
+                        name: results.name,
+                        address: results.formatted_address,
+                        rating: results.rating
+                    });
+                    console.log(places)
+                }
+                if (allDetailsRequestsComplete()) {
+                    console.log('all details are done now. proceed.')
 
-          getTravelTime();
+                    getTravelTime();
+                }
+            });
         }
-      });
+        console.log('SearchCallback, places:', places);
     }
-    console.log('SearchCallback, places:', places);
-  }
 }
 
 function getTravelTime() {
-  var origin = currentAdd;
+    var origin = currentAdd;
 
-  //Pushing address values into destinations array for distance matrix calculations
-  var destination = [];
-  for (var h = 0; h < places.length; h++) {
-    destination.push(places[h].address);
-  }
-
-  var selectTransporatation = document.getElementById('methodTransportation');
-  var userSelectMode =
-    selectTransporatation.options[selectTransporatation.selectedIndex].value;
-
-  var service2 = new google.maps.DistanceMatrixService();
-  service2.getDistanceMatrix(
-    {
-      origins: [origin],
-      destinations: destination,
-
-      travelMode: userSelectMode,
-      unitSystem: google.maps.UnitSystem.METRIC,
-      avoidHighways: false,
-      avoidTolls: false
-    },
-    function(response, status) {
-      if (status !== 'OK') {
-        alert('Error was: ' + status);
-      } else {
-        var originList = response.originAddresses;
-
-        for (var i = 0; i < originList.length; i++) {
-          var results = response.rows[i].elements;
-
-          for (var j = 0; j < results.length; j++) {
-          
-
-            console.log(places[j].name)
-            console.log(results[j].duration.text)
-            console.log(results[j].distance.text)
-            var newRow = $("<tr>").append(
-              $("<td>").text(places[j].name),
-              $("<td>").text(places[j].price),
-              $("<td>").text(places[j].rating),
-              $("<td>").text(results[j].duration.text),
-              $("<td>").text(places[j].address),
-              
-            );
-            //Appending all of new div to html of page
-            $("#resultsDisplay").append(newRow)
-
-          }
-        }
-      }
+    //Pushing address values into destinations array for distance matrix calculations
+    var destination = [];
+    for (var h = 0; h < places.length; h++) {
+        destination.push(places[h].address);
     }
-  );
+
+    var selectTransporatation = document.getElementById('methodTransportation');
+    var userSelectMode =
+        selectTransporatation.options[selectTransporatation.selectedIndex].value;
+
+    var service2 = new google.maps.DistanceMatrixService();
+    service2.getDistanceMatrix({
+            origins: [origin],
+            destinations: destination,
+
+            travelMode: userSelectMode,
+            unitSystem: google.maps.UnitSystem.METRIC,
+            avoidHighways: false,
+            avoidTolls: false
+        },
+        function(response, status) {
+            if (status !== 'OK') {
+                alert('Error was: ' + status);
+            } else {
+                var originList = response.originAddresses;
+
+                for (var i = 0; i < originList.length; i++) {
+                    var results = response.rows[i].elements;
+
+                    for (var j = 0; j < results.length; j++) {
+
+
+                        console.log(places[j].name)
+                        console.log(results[j].duration.text)
+                        console.log(results[j].distance.text)
+                        var newRow = $("<tr>").append(
+                            $("<td>").text(places[j].name),
+                            $("<td>").text(places[j].price),
+                            $("<td>").text(places[j].rating),
+                            $("<td>").text(results[j].duration.text),
+                            $("<td>").text(places[j].address),
+
+                        );
+                        //Appending all of new div to html of page
+                        $("#resultsDisplay").append(newRow)
+
+                    }
+                }
+            }
+        }
+    );
 };
 
 function geocodeAddress(address, callback) {
-  // var geocoder = new google.maps.Geocoder();
-  geocoder.geocode({ address: address }, function(results, status) {
-    if (status === 'OK') {
-      let currentLatLng = results[0].geometry.location;
-      console.log('Geocoding: ', {lat: currentLatLng.lat(), lng: currentLatLng.lng() });
-      callback(currentLatLng);
-    } else alert('Geocode was not successful for the following reason: ' + status);
-  });
+    // var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: address }, function(results, status) {
+        if (status === 'OK') {
+            let currentLatLng = results[0].geometry.location;
+            console.log('Geocoding: ', { lat: currentLatLng.lat(), lng: currentLatLng.lng() });
+            callback(currentLatLng);
+        } else alert('Geocode was not successful for the following reason: ' + status);
+    });
 }
