d98a6ff878448d252c583d96648ddabd5af98f93
Mike Kravtsov <mike4040@me.com>
2019-09-18 08:52:54 -0700

    Cleaning CallBack Hell

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,13 +18,13 @@ function initMap() {
   };
 
   service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, searchCallback);
+  // service.nearbySearch(request, searchCallback);
 
   //Call geocoder function and center on submit once address placed
   var geocoder = new google.maps.Geocoder();
 
   //submit id will be replaced by id of submit button on form
-  document.getElementById('submit').addEventListener('click', function () {
+  document.getElementById('submit').addEventListener('click', function() {
     // geocodeAddress(geocoder, map);
   });
 
@@ -32,96 +32,88 @@ function initMap() {
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
 
-
   // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function () {
+  map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
   });
-
 
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
 
-  searchBox.addListener('places_changed', function () {
+  searchBox.addListener('places_changed', function() {
     let currentLocInput = searchBox.getPlaces();
 
-    if (currentLocInput.length == 0)
-      return;
+    if (currentLocInput.length == 0) return;
 
     currentAdd = currentLocInput[0].formatted_address;
     let currentGeo = currentLocInput[0].geometry;
     let bounds = new google.maps.LatLngBounds();
-    if (currentGeo.viewport)
-      bounds.union(currentGeo.viewport);
-    else
-      bounds.extend(currentGeo.location);
+    if (currentGeo.viewport) bounds.union(currentGeo.viewport);
+    else bounds.extend(currentGeo.location);
     map.fitBounds(bounds);
 
-
     geocodeAddress(currentAdd, callSearchNearby);
-      
-      
-      var origin = currentAdd
-      
-      //Pushing address values into destinations array for distance matrix calculations
-      var destination = [];
-      for (var h = 0; h < places.length; h++) {
-        destination.push(places[h].address)
-      }
-      
-      var selectTransporatation = document.getElementById("methodTransportation")
-      var userSelectMode = selectTransporatation.options[selectTransporatation.selectedIndex].value;
-      
-      var service2 = new google.maps.DistanceMatrixService;
-      service2.getDistanceMatrix({
+
+    var origin = currentAdd;
+
+    //Pushing address values into destinations array for distance matrix calculations
+    var destination = [];
+    for (var h = 0; h < places.length; h++) {
+      destination.push(places[h].address);
+    }
+
+    var selectTransporatation = document.getElementById('methodTransportation');
+    var userSelectMode =
+      selectTransporatation.options[selectTransporatation.selectedIndex].value;
+
+    var service2 = new google.maps.DistanceMatrixService();
+    service2.getDistanceMatrix(
+      {
         origins: [origin],
         destinations: destination,
-        
+
         travelMode: userSelectMode,
         unitSystem: google.maps.UnitSystem.METRIC,
         avoidHighways: false,
         avoidTolls: false
-      }, function (response, status) {
-        
+      },
+      function(response, status) {
         if (status !== 'OK') {
           alert('Error was: ' + status);
         } else {
           var originList = response.originAddresses;
-          
+
           for (var i = 0; i < originList.length; i++) {
             var results = response.rows[i].elements;
-            geocoder.geocode({ 'address': originList[i] }, function(){});
+            geocoder.geocode({ address: originList[i] }, function() {});
             for (var j = 0; j < results.length; j++) {
-              geocoder.geocode({ 'address': destination[j] }, function(){});
-              
-              console.log(places[j].name)
-              console.log(results[j].duration.text)
-              console.log(results[j].distance.text)
-              
+              geocoder.geocode({ address: destination[j] }, function() {});
+
+              console.log(places[j].name);
+              console.log(results[j].duration.text);
+              console.log(results[j].distance.text);
             }
           }
-          
         }
-      });
-      
-    });
-    
-  }
-  
-  function callSearchNearby(latLng) {
+      }
+    );
+  });
+}
+
+function callSearchNearby(latLng) {
   let requestNearby = {
     location: latLng,
     radius: '500',
     type: ['restaurant']
   };
-  service.nearbySearch(requestNearby, searchCallback);
-  }
+  service.nearbySearch(requestNearby, callGetDetails);
+}
 
-  function searchCallback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        let request = {
-          placeId: results[i].place_id,
+function callGetDetails(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      let request = {
+        placeId: results[i].place_id,
         fields: ['name', 'rating', 'formatted_address']
       };
       service.getDetails(request, makeDetailCallback(places));
@@ -139,18 +131,16 @@ function makeDetailCallback(placesToModify) {
         address: results.formatted_address,
         rating: results.rating
       });
-  }
+  };
 }
 
 function geocodeAddress(address, callback) {
   var geocoder = new google.maps.Geocoder();
-  geocoder.geocode({ 'address': address }, function (results, status) {
+  geocoder.geocode({ address: address }, function(results, status) {
     if (status === 'OK') {
       currentLatLng = results[0].geometry.location;
       console.log('Geocoding: ', currentLatLng);
       callback(currentLatLng);
-    }
-    else
-      alert('Geocode was not successful for the following reason: ' + status);
+    } else alert('Geocode was not successful for the following reason: ' + status);
   });
 }
